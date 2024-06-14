8f8b02de56251f402c97d571dd6e3b6d4ec8df30
icep0ps <tapsmuko@gmail.com>
2024-06-14 15:48:40 +0200

    feat: display dialog on game over

--- public/javascript/classes/Display.ts
+++ public/javascript/classes/Display.ts
@@ -1,115 +1,115 @@
 import Ship from './Ship';
-import Board from './Board';
 import Game from './Game';
-import Coordiante from './Coordinate';
 
-export default class DisplayController {
-  render = {
-    ship(boardid: string, coordiantes: string[]) {
-      coordiantes.forEach((coordiante) => {
-        const board = document.getElementById(boardid);
+export default class Display {
+  game: Game;
 
-        if (!board) return;
+  constructor(game: Game) {
+    this.game = game;
+  }
 
-        const grid = board.querySelector(
-          `[data-coordinates="${coordiante[0]},${coordiante[2]}"]`
-        );
-        if (grid) grid.classList.add('ship');
-      });
-    },
-    gameboards() {
-      const playerboard = DisplayController.create.board(
-        'player',
-        Game.players.player.board
-      );
-      const enemyboard = DisplayController.create.board(
-        'enemy',
-        Game.players.enemy.board
-      );
-    },
-    setupboard() {
-      const setupboard = DisplayController.create.board(
-        'setup',
-        Game.players.player.board
-      );
+  ship(boardid: string, coordiantes: string[]) {
+    coordiantes.forEach((coordiante) => {
+      const board = document.getElementById(boardid);
 
-      return setupboard;
-    },
-    attack(enemy: string, coordiante: string, isHit: boolean) {
-      const board = document.getElementById(enemy);
       if (!board) return;
 
       const grid = board.querySelector(
         `[data-coordinates="${coordiante[0]},${coordiante[2]}"]`
       );
+      if (grid) grid.classList.add('ship');
+    });
+  }
 
-      if (isHit) grid?.classList.add('hit');
-      else grid?.classList.add('miss');
-    },
+  showGameoverDialog(message: string) {
+    const dialog = document.querySelector('dialog');
+    dialog?.showModal();
+
+    const paragraph = document.querySelector('dialog p');
+    if (paragraph) paragraph.textContent = message;
+
+    document.querySelector('dialog button')?.addEventListener('click', () => {
+      window.location.reload();
+      dialog?.close();
+    });
+  }
+
+  gameboards = () => {
+    const playerboard = this.game.controllers.display.createBoard(
+      'player',
+      this.game.state.players.player.board.size,
+      'Your board'
+    );
+    const enemyboard = this.game.controllers.display.createBoard(
+      'enemy',
+      this.game.state.players.enemy.board.size,
+      'Enemy board'
+    );
+
+    return { player: playerboard, enemy: enemyboard };
   };
 
-  highlight = {
-    add(event: MouseEvent) {
-      if (Game.players.player.board.allShipsArePlaced()) return;
+  attack(enemy: string, coordiante: string, isHit: boolean) {
+    const board = document.getElementById(enemy);
+    if (!board) return;
 
-      const grid = event.currentTarget as HTMLDivElement;
-      const grids = DisplayController.getShipGrids(
-        grid,
-        Game.players.player.board.ships[Game.players.player.board.placedships]
-      );
+    const grid = board.querySelector(
+      `[data-coordinates="${coordiante[0]},${coordiante[2]}"]`
+    );
 
-      if (DisplayController.isValidPlacement(grids))
-        grids.forEach((grid) => grid.classList.add('highlight'));
-    },
+    if (isHit) grid?.setAttribute('class', 'hit');
+    else grid?.setAttribute('class', 'miss');
+  }
 
-    remove(event: MouseEvent) {
-      if (Game.players.player.board.allShipsArePlaced()) return;
-
-      const grid = event.currentTarget as HTMLDivElement;
-
-      const grids = DisplayController.getShipGrids(
-        grid,
-        Game.players.player.board.ships[Game.players.player.board.placedships]
-      );
-
-      grids.forEach((grid) => grid.classList.remove('highlight'));
-    },
-  };
-
-  static isValidPlacement(grids: HTMLDivElement[]) {
-    const board = Game.players.player.board;
+  isValidPlacement(grids: HTMLDivElement[]) {
+    const board = this.game.state.players.player.board;
     return grids.length === board.ships[board.placedships].size;
   }
 
-  static create = {
-    board(id: string, Board: Board) {
-      const board = document.createElement('div');
-      board.classList.add('board');
-      board.setAttribute('id', id);
+  createBoard = (id: string, size: number, boardTitle: string) => {
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'I', 'J'];
 
-      const main = document.getElementsByTagName('main')[0];
-      main.appendChild(board);
+    const main = document.getElementsByTagName('section')[0];
 
-      for (let x = 0; x < Board.size; x++) {
-        for (let y = 0; y < Board.size; y++) {
-          const grid = DisplayController.create.grids(
-            x.toString() + ',' + y.toString()
-          );
-          if (board) board.appendChild(grid);
-        }
+    const wrapper = this.createElement('div', { className: 'wrapper' }, main);
+
+    const player = this.createElement('h1', null, wrapper);
+    player.innerText = boardTitle;
+
+    const board = this.createElement(
+      'div',
+      { className: 'board', id },
+      wrapper
+    );
+
+    if (id === 'enemy') {
+      const options = this.createElement('div', { id: 'options' }, board);
+
+      const startBtn = this.createElement('button', null, options);
+      startBtn.innerText = 'Play';
+      startBtn.addEventListener('click', () => this.game.start(board));
+    }
+
+    for (let x = 0; x < size; x++) {
+      for (let y = 0; y < size; y++) {
+        const grid = this.game.controllers.display.createGrids(
+          x.toString() + ',' + y.toString()
+        );
+        if (board) board.appendChild(grid);
       }
+    }
 
-      return board;
-    },
-    grids(coordinates: string) {
-      const grid = document.createElement('div');
-      grid.classList.add('grid');
-      grid.dataset.coordinates = coordinates;
-      return grid;
-    },
+    return board;
   };
 
-  static getShipGrids(grid: HTMLDivElement, ship: Ship) {
+  createGrids(coordinates: string) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.dataset.coordinates = coordinates;
+    return grid;
+  }
+
+  getShipGrids(grid: HTMLDivElement, ship: Ship) {
     const coordinates = grid.dataset.coordinates as string;
     const grids = [];
     for (let i = 0; i < ship.size; i++) {
@@ -123,5 +123,23 @@ export default class DisplayController {
     }
 
     return grids as HTMLDivElement[];
+  }
+
+  createElement(
+    tagName: keyof HTMLElementTagNameMap,
+    options: {
+      className?: string;
+      id?: string;
+    } | null,
+    parent?: HTMLElement
+  ) {
+    const element = document.createElement(tagName);
+    if (options) {
+      if (options.id) element.setAttribute('id', options.id);
+      if (options.className) element.classList.add(options.className);
+    }
+
+    if (parent) parent.appendChild(element);
+    return element;
   }
 }
